2e92e8502d68c823d7603225d8b796ef6b1c54fe
Bobbe Cooper <bobbe.cooper@gmail.com>
2020-03-20 11:22:57 -0400

    initial code commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# main input loop
+
+def update_cycles(cycles, user_input):
+    pass
+
+
+def print_new_idea(cycles):
+    pass
+
+
+def print_options(cycles):
+    pass
+
+
+cycles = []
+
+print("Enter a number to go somewhere; 0 to quit")
+user_input = int(input())
+
+while user_input != 0:
+    update_cycles(cycles, user_input)
+    print_new_idea(cycles)
+    print_options(cycles)
+    user_input = int(input())
